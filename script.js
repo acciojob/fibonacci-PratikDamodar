48bd794b517cbe5a7806e3d7f0787c93d63085b4
PratikDamodar <117561545+PratikDamodar@users.noreply.github.com>
2023-04-17 13:41:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+ if (num <= 1) {
+    return num;
+  }
+
+  let prev1 = 0;
+  let prev2 = 1;
+  let current = 0;
+
+  for (let i = 2; i <= num; i++) {
+    current = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = current;
+  }
+
+  return current;
 }
 
 module.exports = fibonacci;
